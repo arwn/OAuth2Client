031cb5c9bdec992feb500802feb2274dbd9f5dca
Aren R Windham <windham.aren@protonmail.com>
2019-12-30 18:40:12 -0800

    reformatted functions to be readable

--- src/OAuth2Client.jl
+++ src/OAuth2Client.jl
@@ -11,24 +11,25 @@ struct Client
 end
 
 # Gets a token using the 'Client Credentials' flow
-gettoken(c::Client) =
-    let d = HTTP.request(:POST, c.host * "/oauth/token", [],
-                "grant_type=client_credentials&" *
-                "client_id=$(c.id)&" *
-                "client_secret=$(c.secret)").body |>
-            String |>
-            JSON.parse
-        Client(
-            c.host,
-            c.id,
-            c.secret,
-            d["access_token"])
-    end
+function gettoken(c::Client)
+    req = HTTP.request(:POST, c.host * "/oauth/token", [],
+        "grant_type=client_credentials&" *
+        "client_id=$(c.id)&" *
+        "client_secret=$(c.secret)")
+    s = String(req.body)
+    j = JSON.parse(s)
+    return Client(
+        c.host,
+        c.id,
+        c.secret,
+        j["access_token"])
+end
 
-request(c::Client, method, endpoint::String) =
-    HTTP.request(method, c.host * endpoint, 
-        ["Authorization" => "Bearer " * c.token]).body |>
-    String |>
-    JSON.parse
+function request(c::Client, method, endpoint::String)
+    req = HTTP.request(method, c.host * endpoint, 
+        ["Authorization" => "Bearer " * c.token])
+    s = String(req.body)
+    return JSON.parse(s)
+end
 
 end # module
