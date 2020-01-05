f734f2be7be63a7a39fd7b724c2d859456076470
Aren R Windham <windham.aren@protonmail.com>
2020-01-04 21:19:19 -0800

    add optional delay to requests

--- src/OAuth2Client.jl
+++ src/OAuth2Client.jl
@@ -2,16 +2,35 @@ module OAuth2Client
 
 import HTTP
 import JSON
+import Dates
 
-struct Client
+mutable struct Client
     host::String
     id::String
     secret::String
     token::Union{Nothing, String}
+    cooldown::Dates.Second
+    lastcall::Dates.DateTime
+end
+
+Client(host::String, id::String,
+        secret::String; cooldown::Dates.Second=Dates.Second(0)) =
+    Client(host, id, secret, nothing, cooldown, Dates.DateTime(1))
+
+# handles api rate limits
+function _delay!(c::Client)
+    if c.cooldown == 0 return end
+    now = Dates.now()
+    diff = now - c.lastcall
+    if diff < Dates.Second(c.cooldown)
+        sleep((c.cooldown - diff).periods[1])
+    end
+    c.lastcall = Dates.now()
 end
 
 # Gets a token using the 'Client Credentials' flow
 function gettoken(c::Client)
+    _delay!(c)
     req = HTTP.request(:POST, c.host * "/oauth/token", [],
         "grant_type=client_credentials&" *
         "client_id=$(c.id)&" *
@@ -22,10 +41,13 @@ function gettoken(c::Client)
         c.host,
         c.id,
         c.secret,
-        j["access_token"])
+        j["access_token"],
+        c.cooldown,
+        c.lastcall)
 end
 
 function request(c::Client, method, endpoint::String)
+    _delay!(c)
     req = HTTP.request(method, c.host * endpoint, 
         ["Authorization" => "Bearer " * c.token])
     s = String(req.body)
